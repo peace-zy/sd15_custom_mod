e6f4f366138bd0a2e85db05f19bbe120f50f328d
shaoping <260259423@qq.com>
2025-03-09 10:59:08 +0800

    Create infer.sh

---
+++ infer_script/infer.sh
@@ -0,0 +1,5 @@
+python infer_sd15_add_cond_lpw.py \
+   --unet_path '../out_model/checkpoint-45000/unet' \
+   --width 768 \
+   --height 768 \
+   --save_dir '../infer_result/multi_scale/random_5csv_ckpt_50000_768_768_add_cond'
